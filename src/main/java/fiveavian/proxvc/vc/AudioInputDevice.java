1ae31ba801741a6d30300ff1ae5b111a338fe8d7
IsLocal <samal1and2@gmail.com>
2025-07-10 12:23:14 -0400

    final touches.

--- src/main/java/fiveavian/proxvc/vc/AudioInputDevice.java
+++ src/main/java/fiveavian/proxvc/vc/AudioInputDevice.java
@@ -1,10 +1,7 @@
 package fiveavian.proxvc.vc;
 
 import org.lwjgl.BufferUtils;
-import org.lwjgl.openal.AL10;
-import org.lwjgl.openal.ALC10;
-import org.lwjgl.openal.ALC11;
-import org.lwjgl.openal.ALUtil;
+import org.lwjgl.openal.*;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
